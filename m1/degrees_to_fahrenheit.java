0665b4821d038165d418e0cb23d9935aeb06d53a
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-27 20:55:26 -0700

    degree to fh

---
+++ m1/degrees_to_fahrenheit.java
@@ -0,0 +1,8 @@
+class degrees_to_fahrenheit{
+	public static void main(String[] args){
+	double degrees = 10;
+	System.out.println("temp in degrees"+degrees);
+	int fahrenheit = (10*9/5)+32;
+	System.out.println("new fahrenheit"+fahrenheit);
+    }
+}
