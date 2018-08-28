742bcf46986f20548fb1563d8bc815eb5771d1ae
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-27 18:07:16 -0700

    q and r

---
+++ m1/quotient_remainder.java
@@ -0,0 +1,12 @@
+class quotient_remainder
+{
+    public static void main(String[] args){
+    int divident = 20 ;
+    int divisor = 5;
+
+    int quotient = divident/divisor;
+    int remainder = divident % divisor;
+    System.out.println("req quotient"+quotient);
+    System.out.println("req remainder"+remainder);
+    }
+}
