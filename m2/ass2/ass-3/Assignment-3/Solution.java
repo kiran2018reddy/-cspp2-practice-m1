310f083312f03eb6fff27f09793d339b164b7374
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-31 14:49:06 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m2/ass2/ass-3/Assignment-3/Solution.java
+++ m2/ass2/ass-3/Assignment-3/Solution.java
@@ -6,21 +6,25 @@ final class Solution {
 	Do not modify this main function.
 	*/
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+		Scanner s = new Scanner(System.in);      
         long base = s.nextInt();
         long exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base , exponent);
         System.out.println(result);
 	}
-
+/**
+ * { function_description }
+ *
+ * @param      base      The base
+ * @param      exponent  The exponent
+ *
+ * @return     { description_of_the_return_value }
+ */
 	public static long power(final long base, final long exponent) {
-		if (exponent > 0)
-		{
-			return (base*power(base, exponent - 1));
+		if (exponent > 0){
+			return (base * power(base, exponent - 1));
 		}
 		return 1;
 	}
-	/*
-	Need to write the power function and print the output.
-	*/
+	
 }
