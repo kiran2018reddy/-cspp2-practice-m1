940193783e0a321e4fd81e34a2534ce88f50b692
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-31 14:50:22 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m2/ass2/ass-3/Assignment-3/Solution.java
+++ m2/ass2/ass-3/Assignment-3/Solution.java
@@ -9,11 +9,11 @@ final class Solution {
 		Scanner s = new Scanner(System.in);      
         long base = s.nextInt();
         long exponent = s.nextInt();
-        long result = power(base , exponent);
+        long result = power(base, exponent);
         System.out.println(result);
 	}
 /**
- * { function_description }
+ * { function_description }.
  *
  * @param      base      The base
  * @param      exponent  The exponent
@@ -21,10 +21,9 @@ final class Solution {
  * @return     { description_of_the_return_value }
  */
 	public static long power(final long base, final long exponent) {
-		if (exponent > 0){
+		if (exponent > 0) {
 			return (base * power(base, exponent - 1));
 		}
 		return 1;
 	}
-	
 }
