0469438080217d5c71241479695b9d544e6e9c02
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-31 14:41:33 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/ass2/Assignment-2/Solution.java
+++ m2/ass2/Assignment-2/Solution.java
@@ -16,14 +16,13 @@ public final class Solution {
  *
  * @param      args  The arguments
  */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
         roots(a, b, c);
     }
-    
      /**
      * { function_description }.
      *
