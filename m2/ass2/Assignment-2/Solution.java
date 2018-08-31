85484603fde27176bf2fb3531b45aa42f0e7f8b3
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-31 14:28:44 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/ass2/Assignment-2/Solution.java
+++ m2/ass2/Assignment-2/Solution.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
+/**
+ * { item_description }
+ */
 import java.lang.Math.*;
+/**
+ * { item_description }
+ */
 
 public final class Solution {
     // object takingprivate Solution() { }
-
-
-    /*
-    Do not modify this main function.
-    */
     public static void main(String[] args) {
+        /**
+         * { var_description }
+         */
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
@@ -16,15 +20,22 @@ public final class Solution {
         roots(a, b, c);
     }
     private Solution() { }
+    /**
+     * { function_description }
+     *
+     * @param      x     { parameter_description }
+     * @param      y     { parameter_description }
+     * @param      z     { parameter_description }
+     */
     public static void roots(final int x, final int y, final int z) {
         double rot1;
         double rot2;
         double quad;
         final int b = 4;
         quad = Math.sqrt((y*y)-(b*x*z));
-        rot1 = (-y+quad) / (2*x);
-        rot2 = (-y-quad) / (2*x);
-        System.out.println(rot1+" "+rot2);
+        rot1 = (-y + quad) / (2*x);
+        rot2 = (-y - quad) / (2*x);
+        System.out.println(rot1 +" "+ rot2);
     }
 }
     /*
